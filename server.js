eb9f77a7212bcf51b787edc6b27513e217b01138
Mona Lisa <naomiroznik@gmail.com>
2022-11-18 13:39:48 -0500

    function to delete note

--- server.js
+++ server.js
@@ -10,10 +10,27 @@ const allNotes = require('./db/db.json');
 app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 function createNewNote(body, notesArray) {
     const newNote = body;
-    if (!Array.isArray(notesArray))
+    if (!Array.isArray(notesArray)) {
         notesArray = [];
 
         if (notesArray.length === 0)
@@ -29,19 +46,26 @@ function createNewNote(body, notesArray) {
         );
         return newNote;
     }
+}
 
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, './public/index.html'));
-    });
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray)
+            );
+            break;
+        }
+    }
+}
 
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, './public/notes.html'));
-    });
-
-
-
-
-
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
