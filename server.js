9bb3f52dbc076b143555be1083003d7a3d35ffea
Mona Lisa <naomiroznik@gmail.com>
2022-11-18 13:17:06 -0500

    adding middleware

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
-// const notes = require('./db/db');
+const notes = require('./db/db.json');
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+    fs.writeFileSync (
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify ({allNotes: notesArray})
+    );
+    return newNote;
+}
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
