b0105d37dab3bf9cf383ab4e0a4420769aa15838
Mona Lisa <naomiroznik@gmail.com>
2022-11-18 13:26:13 -0500

    created routes to server front end

--- server.js
+++ server.js
@@ -30,6 +30,14 @@ function createNewNote(body, notesArray) {
         return newNote;
     }
 
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/notes.html'));
+    });
+
 
 
 
