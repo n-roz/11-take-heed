7b196212f8263d6a18c93f992d1dc9844c2d84e3
Mona Lisa <naomiroznik@gmail.com>
2022-12-01 14:48:33 -0500

    still wont work but this has notes from askbcs

--- server.js
+++ server.js
@@ -28,25 +28,86 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray)) {
-        notesArray = [];
+// function createNewNote(body, notesArray) {
+//     const newNote = body;
+//     if (!Array.isArray(notesArray)) {
+//         notesArray = [];
 
-        if (notesArray.length === 0)
-            notesArray.push(0);
+//         if (notesArray.length === 0)
+//             notesArray.push(0);
 
-        body.id = notesArray[0];
-        notesArray[0]++;
+//         body.id = notesArray[0];
+//         notesArray[0]++;
 
-        notesArray.push(newNote);
-        fs.writeFileSync (
-            path.join(__dirname, './db/db.json'),
-            JSON.stringify(notesArray, null, 2)
-        );
-        return newNote;
+//         notesArray.push(newNote);
+//         fs.writeFileSync (
+//             path.join(__dirname, './db/db.json'),
+//             JSON.stringify(notesArray, null, 2)
+//         );
+//         return newNote;
+//     }
+// }
+
+// new createnewnote function
+read() {
+    return readFileAsync('db/db.json', 'utf8');
+  }
+
+  write(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      // If notes isn't an array or can't be turned into one, send back a new empty array
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
     }
-}
+
+    // Add a unique id to the note using uuid package
+    const newNote = { title, text, id: uuidv1() };
+
+    // Get all notes, add the new note, write all the updated notes, return the newNote
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+  // end newnote function
+
+//   router.post('/notes', (req, res) => {
+//     store
+//       .addNote(req.body)
+//       .then((note) => res.json(note))
+//       .catch((err) => res.status(500).json(err));
+//   });
+
+
+
+// So the steps you want to make sure that youre doing is first Getting the notes from the db.json using a read() function
+// 1:35
+// then turn those results into a array
+// 1:36
+// then we add our newNote to the array and take what is called updatedNotes and write then back into the db.json
+// 1:37
+// in the post route you can ignore the store part, thats only if you are using them from a separate file that put all of these functions into a class
+// 1:38
+// So I would really focus on restructuring your createNewNote function a little to follow that pattern, but you have the right idea since you are able to access the body with your const newNote!
 
 app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, allNotes);
