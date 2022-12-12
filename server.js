43030725fd9b1356b4f314f838cb029797a44126
Mona Lisa <naomiroznik@gmail.com>
2022-12-12 12:10:40 -0500

    routes and creatnewnote function in server.js, need to finish adding unique id to note

--- server.js
+++ server.js
@@ -22,105 +22,54 @@ app.use(express.static('public'));
 // // 1:38
 // // So I would really focus on restructuring your createNewNote function a little to follow that pattern, but you have the right idea since you are able to access the body with your const newNote!
 
+//     // Add a unique id to the note using uuid package
+//     const note = { title, text, id: uuidv1() };
+
 function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
     path.join(__dirname, './db/db.json'),
     JSON.stringify(notesArray, null, 2)
-);
-        return note;
-    };
+  );
+  return note;
+};
 
 // routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// GET /notes should return the notes.html file
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('/api/notes', (req, res) => { 
-res.json(note);
+// GET * should return the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// GET /api/notes should read the db.json file and return all saved notes as JSON
+app.get('/api/notes', (req, res) => {
+  res.json(note);
+});
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
+// You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req, res) => {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
+  const note = createNewNote(req.body, notes);
+  res.json(note);
 });
 
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
+// In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, 
+// and then rewrite the notes to the db.json file.
 // this one needs work
 // app.delete('/api/notes/:id', (req, res) => {
 //     const deleteNote =
 //     res.json(true);
 // });
-
-
-
-// //         if (notesArray.length === 0)
-// //             notesArray.push(0);
-
-// //         body.id = notesArray[0];
-// //         notesArray[0]++;
-
-// //         notesArray.push(newNote);
-// //         fs.writeFileSync (
-// //             path.join(__dirname, './db/db.json'),
-// //             JSON.stringify(notesArray, null, 2)
-// //         );
-// //         return newNote;
-// //     }
-// // }
-
-// // new createnewnote function
-// read() {
-//     return readFileAsync('db/db.json', 'utf8');
-//   }
-
-//   write(note) {
-//     return writeFileAsync('db/db.json', JSON.stringify(note));
-//   }
-
-//   getNotes() {
-//     return this.read().then((notes) => {
-//       let parsedNotes;
-
-//       // If notes isn't an array or can't be turned into one, send back a new empty array
-//       try {
-//         parsedNotes = [].concat(JSON.parse(notes));
-//       } catch (err) {
-//         parsedNotes = [];
-//       }
-
-//       return parsedNotes;
-//     });
-//   }
-
-//   addNote(note) {
-//     const { title, text } = note;
-
-//     if (!title || !text) {
-//       throw new Error("Note 'title' and 'text' cannot be blank");
-//     }
-
-//     // Add a unique id to the note using uuid package
-//     const newNote = { title, text, id: uuidv1() };
-
-//     // Get all notes, add the new note, write all the updated notes, return the newNote
-//     return this.getNotes()
-//       .then((notes) => [...notes, newNote])
-//       .then((updatedNotes) => this.write(updatedNotes))
-//       .then(() => newNote);
-//   }
-//   // end newnote function
-
-// //   router.post('/notes', (req, res) => {
-// //     store
-// //       .addNote(req.body)
-// //       .then((note) => res.json(note))
-// //       .catch((err) => res.status(500).json(err));
-// //   });
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
